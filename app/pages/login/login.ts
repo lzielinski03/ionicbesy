d1949524856f0fbb98d0bb81537ed429b456cec0
lzielinski <lzielinski03@gmail.com>
2016-07-19 14:44:20 -0300

    login funcionando

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,11 +1,10 @@
-import {Component, provide} from '@angular/core';
-import {NavController, Storage, LocalStorage} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Alert} from 'ionic-angular';
 import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from '@angular/common';
 import {LoginValidator} from './validators/loginValidator';
 
-import {Http, Headers, HTTP_BINDINGS} from '@angular/http';
+import {Http, HTTP_BINDINGS} from '@angular/http';
 import {AuthService} from './../../services/auth/auth';
-import {AuthHttp, AuthConfig, JwtHelper} from 'angular2-jwt';
 
 import 'rxjs/add/operator/map';
 
@@ -16,15 +15,7 @@ import {LegajoList} from './../legajos/legajo-list'
 @Component({
 	templateUrl: 'build/pages/login/login.html',
 	directives: [FORM_DIRECTIVES],
-    providers: [
-    provide(AuthHttp, {
-      useFactory: (http) => {
-      return new AuthHttp(new AuthConfig, http);
-      },
-      deps: [Http]
-    }),
-    HTTP_BINDINGS
-  ]
+    providers: [HTTP_BINDINGS, AuthService]
 })
 export class LoginPage {
 
@@ -32,25 +23,7 @@ export class LoginPage {
     username: AbstractControl;
     password: AbstractControl;
 
-    LOGIN_URL: string = "http://localhost:8090/auth/login";
-    //SIGNUP_URL: string = "http://localhost:8090/users";
-    
-    local: Storage = new Storage(LocalStorage);
-    loggedUser: string;
-    contentHeader : Headers = new Headers({"Content-Type": "application/json"});
-    jwtHelper: JwtHelper = new JwtHelper();
-
-    /*
-	
-	auth: AuthService;
-    //authType: string = 'login';
-    
-    //error: string;
-
-    */
-    
-
-    constructor(public nav: NavController, private fb: FormBuilder, private http: Http) {
+    constructor(public nav: NavController, private fb: FormBuilder, private http: Http, private authService:AuthService) {
     	this.authForm = fb.group({  
             'username': ['', Validators.compose([Validators.required, Validators.minLength(8), LoginValidator.checkFirstCharacterValidator])],
             'password': ['', Validators.compose([Validators.required, Validators.minLength(8), LoginValidator.checkFirstCharacterValidator])]
@@ -58,89 +31,20 @@ export class LoginPage {
 
     	this.username = this.authForm.controls['username'];
     	this.password = this.authForm.controls['password'];
+    }
+
+    login(event:Event): void {
         
-        /*
-        this.nav = nav;
-        this.auth = AuthService;
-        this.local.get('profile').then(profile => {
-            this.user = JSON.parse(profile);
-        }).catch(error => {
-            console.log(error);
-        });*/
-    }
-
-    onSubmit(value: string): void { 
+        event.preventDefault();
         if(this.authForm.valid) {
-            console.log('Submitted value: ', value);
+            
+            this.authService.login(this.username.value, this.password.value)
+                .subscribe(() => 
+                    this.nav.setRoot(TabsPage),
+                    (alert:any) => this.nav.present(alert)
+                );
         }
     }
-
-    login(credentials: string): void {
-        if(this.authForm.valid) {
-            console.log(credentials);
-            console.log(JSON.stringify(credentials));
-
-            this.http.post(this.LOGIN_URL, JSON.stringify(credentials), {headers: this.contentHeader})
-            .map(res => res.json())
-            .subscribe(
-                data => {console.log(data);this.authSuccess(data.id_token)},
-                err => console.log(err)
-            );
-/*
-            this.http.post(this.LOGIN_URL, JSON.stringify(credentials), { headers: this.contentHeader })
-            .map(res => res.json())
-            .subscribe(
-                data => this.authSuccess(data.id_token),
-                err => console.log(err)
-            );*/
-        }
-    }
-
-    logout() {
-        this.local.remove('id_token');
-        this.loggedUser = null;
-    }
-
-    authSuccess(token) {
-        //this.error = null;
-        this.local.set('id_token', token);
-        this.loggedUser = this.jwtHelper.decodeToken(token).username;
-        this.nav.push(TabsPage);
-    }
-
-/*
-    login(credentials) {
-        console.log(credentials);
-        this.http.post(this.LOGIN_URL, JSON.stringify(credentials), { headers: this.contentHeader })
-        .map(res => res.json())
-        .subscribe(
-            data => this.authSuccess(data.id_token),
-            err => console.log(err)
-        );
-    }
-
-    signup(credentials) {
-        this.http.post(this.SIGNUP_URL, JSON.stringify(credentials), { headers: this.contentHeader })
-        .map(res => res.json())
-        .subscribe(
-            data => this.authSuccess(data.id_token),
-            //err => { this.error = err; console.log(err); }
-            err => console.log(err)
-        );
-    }
-
-    
-
-    authSuccess(token) {
-        //this.error = null;
-        this.local.set('id_token', token);
-        this.user = this.jwtHelper.decodeToken(token).username;
-        this.nav.push(TabsPage);
-    }
-
-    goLegajos() {
-        this.nav.push(TabsPage);
-    }*/
 
     goLegajos() {
       this.nav.push(LegajoList);
--- app/services/auth/auth.ts
+++ app/services/auth/auth.ts
@@ -1,9 +1,66 @@
-import {tokenNotExpired} from 'angular2-jwt';
+import {Injectable,Inject} from '@angular/core';
+import {Http,Headers,Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {StorageUtils} from '../../utils/storage.utils';
+import {Alert} from 'ionic-angular';
+import {User} from '../../classes/user';
 
+//import {tokenNotExpired} from 'angular2-jwt';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
+const CONTENT_TYPE_HEADER:string = 'Content-Type';
+const APPLICATION_JSON:string = 'application/json';
+const BACKEND_URL:string = 'http://localhost:8090/auth/login';
+
+@Injectable()
 export class AuthService {
-	constructor() {}
+	constructor(private http:Http) {}
 
-	public static authenticated() {
-		return tokenNotExpired();
+	login(username:string, password:string):Observable<any> {
+		let headers = new Headers();
+		headers.append(CONTENT_TYPE_HEADER, APPLICATION_JSON);
+		console.log(username);
+		console.log(password);
+		return this.http.post(BACKEND_URL, JSON.stringify({username:username, password:password}), {headers:headers}).map((res:Response) => {
+			let loginData:any = res.json();
+			console.log(res);
+/*
+			console.log(res.ok);
+			console.log(res.status);
+			console.log(loginData);
+*/
+			let user:User = this.readJwt(loginData.token);
+			user.username = username;
+			user.password = password;
+			StorageUtils.setAccount(user);
+			StorageUtils.setToken(loginData.token);
+
+			return user;
+			
+		}).catch((err:any) => {
+			console.log(err);
+			let alert = Alert.create({
+				title: 'Invalid credentials',
+				subTitle: 'You entered invalid credentials !',
+				buttons: ['Ok']
+			});
+			return Observable.throw(alert);
+		});
+	}
+
+	private readJwt(token:string):User {
+        let tokens:Array<any> = token.split('.');
+        let tokenPayload:any = JSON.parse(atob(tokens[1]));
+
+        let user:User = new User();
+        user.lastConnection = new Date();
+        user.email = tokenPayload.sub;
+        //user.id = parseInt(tokenPayload.iss);
+        //user.firstName = tokenPayload.firstName;
+        //user.lastName = tokenPayload.lastName;
+        //user.roles = tokenPayload.role;
+
+        return user;
 	}
 }
